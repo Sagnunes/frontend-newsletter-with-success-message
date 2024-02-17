1929b14395c0f8d104afec8e9f4a5238d5bb79d7
Sérgio Nunes <sagnunes@gmail.com>
2024-02-17 13:31:30 +0000

    added style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      grey: {
+        normal: "hsl(231, 7%, 60%)",
+        charcoal: "hsl(235, 18%, 26%)",
+        darkSlate: "hsl(234, 29%, 20%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+      tomato: "hsl(4, 100%, 67%)",
+    },
   },
   plugins: [],
-}
+};
