f18a11aa19b08b8bc2146129fb36d995016a3774
Sérgio Nunes <sagnunes@gmail.com>
2024-02-18 19:59:32 +0000

    finish all style for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
@@ -14,6 +30,7 @@ module.exports = {
       },
       white: "hsl(0, 0%, 100%)",
       tomato: "hsl(4, 100%, 67%)",
+      orange: "hsl(346, 100%, 66%)",
     },
   },
   plugins: [],
